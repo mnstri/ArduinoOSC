bafb9ceb29c397d8308e61c1785f706750d834f4
Matti Niinimäki <m@mansteri.com>
2023-11-16 18:49:07 +0200

    fixed Arduino Nano RP2040 Connect issues

--- ArduinoOSCWiFi.h
+++ ArduinoOSCWiFi.h
@@ -15,7 +15,7 @@
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-#elif defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKRVIDOR4000) || defined(ARDUINO_SAMD_NANO_33_IOT)
+#elif defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKRVIDOR4000) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT)
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <WiFiUdp.h>
